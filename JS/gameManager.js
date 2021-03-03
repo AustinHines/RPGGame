1c9489ee7fe678791bca72a2d4b1d471acabf452
Austin Hines <Austinhines0605@gmail.com>
2021-03-02 22:31:38 -0600

    Fixed 2nd page picture

--- JS/gameManager.js
+++ JS/gameManager.js
@@ -5,13 +5,13 @@ let GameManager = {
     },
     resetPlayer: function (classType) {
         switch (classType) {
-            case "SSG Goku":
+            case "SSGGoku":
                 player = new Player(classType, 200, 200, 300, 90, 100);
                 break;
-            case "SSGSS Vegeta":
+            case "VegetaSSGSS":
                 player = new Player(classType, 300, 300, 300, 125, 100);
                 break;
-            case "SS Trunks":
+            case "TrunksSS":
                 player = new Player(classType, 150, 200, 100, 50, 90);
                 break;
             case "Picolo":
@@ -19,7 +19,7 @@ let GameManager = {
                 break;
         }
         let getInterface = document.querySelector(".interface");
-        getInterface.innerHTML = '<img src="img/avatar-player/'+ classType.toLowerCase() +'.png>" class="img-avatar"><div><h3>' + classType + '</h3><p>Health:' + player.health + '</p><p>ki:' + player.ki + '</p><p>strength:' + player.strength + '</p><p>agility:' + player.agility + '</p><p>speed:' + player.speed + '</p></div>';
+        getInterface.innerHTML = '<img src="img/avatar-player/'+ classType +'.png" class="img-avatar"><div><h3>' + classType + '</h3><p>Health:' + player.health + '</p><p>ki:' + player.ki + '</p><p>strength:' + player.strength + '</p><p>agility:' + player.agility + '</p><p>speed:' + player.speed + '</p></div>';
 
     },
     setPreFight: function () {
